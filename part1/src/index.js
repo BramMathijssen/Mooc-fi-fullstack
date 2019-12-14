fa118922f526b25991e2a8c7cfa64833017946ca
Bram <Bram.mathijssen@gmail.com>
2019-12-14 23:44:37 +0100

    additions to index

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
+const name = "bert";
+
+//Dit component wordt opgeroepen in de AppTwo function
+const Hello = (props) => (
     <div>
-        <p>Hello world</p>
+        <p>Hello {props.name} from the Hello Component, you are {props.age} years old </p>
     </div>
 )
 
+
+
 const AppTwo = () => {
+    
     console.log('Hello from component')
     const now = new Date();
     const a = 10;
     const b = 20;
+    //const binnen een functie override de globale variable 
+    const name = "BartelJoris"
+    const age = 10
+    
     return (
-        <div>
-            <p>Hello world with a console log element</p>
+        <div id="container">
+            {/* Een naam wordt meegegeven door het props attribuut in de Hello Function */}
+            <Hello name="Bert" />
+            <Hello name="Henk" />
+            <Hello name= "Bertaap" />
+            {/* Dynamisch vullen van het Hello component */}
+            <Hello name= {name} age= {age} />
+            <p>Hello {name} with a console log element </p>
+            <p>The current time is {now.toString()}</p>
+            <br />
             <p>
+                {/* {Alle text binnen curly brackets wordt als js code geintepeteerd.} */}
                 {a} plus {b} is {a + b}
             </p>
         </div>
@@ -36,7 +55,7 @@ let Testlet = () => (
 )
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
 ReactDOM.render(<Test />, document.getElementById('test'))
 ReactDOM.render(<Testlet />, document.getElementById('testlet'))
 ReactDOM.render(<AppTwo />, document.getElementById('apptwo'))
