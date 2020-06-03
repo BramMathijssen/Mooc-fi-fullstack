2ec21c7051946af534da5adbda2a25edf48e437d
Bram <Bram.mathijssen@gmail.com>
2020-06-03 17:41:38 +0200

    exercise 1.3 complete

--- part1/course-info/src/index.js
+++ part1/course-info/src/index.js
@@ -1,21 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+//   const total = exercises1 + exercises2 + exercises3
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content  part1={part1} part2={part2} part3={part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
+//       <Total total={total} />
+//     </div>
+//   )
+// }
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content  part1={part1} part2={part2} part3={part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
-      <Total total={total} />
+      <Content name = {part1.name} exercises = {part1.exercises} />
+      <Content name = {part2.name} exercises = {part2.exercises} />
+      <Content name = {part3.name} exercises = {part3.exercises} />
+      {/* <Content  name={part1.name} name={part2} part3={part3} exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises} /> */}
     </div>
   )
 }
@@ -34,10 +60,7 @@ const Content = (props) => {
   return (
     
     <div>
-      <Part content={props.part1} exercises={props.exercises1} />
-      <Part content={props.part2} exercises={props.exercises2} />
-      <Part content={props.part3} exercises={props.exercises3} />
-      {/* <p>Course content: {props.content}, Amount of exercises {props.exercises}</p> */}
+      <Part name={props.name} exercises={props.exercises} />
     </div>
   )
 }
@@ -45,7 +68,7 @@ const Content = (props) => {
 const Part = (props) => {
   return (
     <div>
-      <p>Course content: {props.content}, Amount of exercises {props.exercises}</p>
+      <p>Course content: {props.name}, Amount of exercises {props.exercises}</p>
     </div>
   )
 }
