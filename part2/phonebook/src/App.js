fa92d3c2ca29acf1e9fa8277859bbf1a711a8621
Bram <Bram.mathijssen@gmail.com>
2020-06-19 23:31:49 +0200

    exercise 2.9 finished

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: 1234
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
 
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterInput, setFilterInput] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -41,14 +42,23 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  // const checkDuplicate = () => {
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setFilterInput(event.target.value)
+  }
 
+  // const isFiltered = function(person) {
+  //   return person.
   // }
 
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filterInput} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson} >
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -62,7 +72,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+        {persons.filter(person =>
+          person.name.toLowerCase().includes(filterInput.toLowerCase()))
+          .map(person =>
+          <p key={person.name}>{person.name} {person.number}</p>)
+      }
       </div>
       <div>debug: {newName}</div>
       ...
